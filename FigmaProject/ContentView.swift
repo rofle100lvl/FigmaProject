05b505856a63e215094ce8df22796ed0af594e65
Горбенко Роман <45801227+rofle100lvl@users.noreply.github.com>
2023-03-14 14:52:31 +0100

    Initial Commit

---
+++ FigmaProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FigmaProject
+//
+//  Created by Roman Gorbenko on 14/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FigmaProject/FigmaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaProjectApp.swift
+//  FigmaProject
+//
+//  Created by Roman Gorbenko on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FigmaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
